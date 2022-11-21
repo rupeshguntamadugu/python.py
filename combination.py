c24ca3bb10612b5c67b20d3228edd95d22c77127
rupesh2020 <guntamadugurupesh@gmail.com>
2022-11-21 11:14:36 +0530

    Adding the combination modified code

--- combination.py
+++ combination.py
@@ -1,7 +1,7 @@
 def comb(L):
-    for p in range(3):
-        for q in range(3):
-            for r in range(3):
+    for p in range(4):
+        for q in range(4):
+            for r in range(4):
                 if (p!=q and q!=r and p!=r):
                     print(L[p], L[q], L[r])
 comb([10, 13, 15])
