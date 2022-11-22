f14188cf31e34684140ec4f66a703034b8c8b858
rupesh2020 <guntamadugurupesh@gmail.com>
2022-11-22 08:42:15 +0530

    Adding reverse a number code

---
+++ reverse.py
@@ -0,0 +1,13 @@
+number = int(input("Enter the integer number: "))  
+  
+revs_number = 0  
+  
+# reverse the integer number using the while loop  
+  
+while (number > 0):  
+    # Logic  
+    remainder = number % 10  
+    revs_number = (revs_number * 10) + remainder  
+    number = number // 10  
+  
+print("The reverse number is : {}".format(revs_number))
